a6e4d2712e4bd01c384553811dc4ade3f307779a
Aleksey Kitaev <axlstarxx@gmail.com>
2021-12-13 01:25:05 +0300

    aftertest2

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ const popUpOpenButton = document.querySelector('.profile__edit-button');
 const popUpClosedButton = document.querySelector('.pop-up__close-button');
 const popUp = document.querySelector('.pop-up');
 
-let formImputName = document.querySelector('.pup-up__edit_name');
-let formImputTitle = document.querySelector('.pup-up__edit_title');
+let formImputName = document.querySelector('.pop-up__edit_name');
+let formImputTitle = document.querySelector('.pop-up__edit_title');
 let imputName = document.querySelector('.profile__name');
 let imputTitle = document.querySelector('.profile__title');
-const svaveEditButton = document.querySelector('.pup-up__save-edit-button');
+const svaveEditButton = document.querySelector('.pop-up__save-edit-button');
 
 function togglePopUp () {
   popUp.classList.toggle('pop-up_opened');
@@ -26,7 +26,7 @@ popUp.addEventListener('mousedown', closePopUpAnyWhere);
 function formSubmitHandler (evt) {
   evt.preventDefault();
   imputName.textContent = formImputName.value;
-  imputTitle.textContent = formImputTitle.value;
+  imputTitle.textContent = formImputTitle.value;0
   togglePopUp ()
 }
 popUp.addEventListener('submit', formSubmitHandler);
